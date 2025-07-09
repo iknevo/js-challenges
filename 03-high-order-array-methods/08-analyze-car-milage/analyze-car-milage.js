03c3150b0067e823a62c147cdf3b27ab3b38931a
idknevo <nevoexists@gmail.com>
2025-07-09 04:48:53 +0300

    03: -> 08

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,16 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((acc, car) => acc + car.mileage, 0);
+  const highestMileage = Math.max(...cars.map((car) => car.mileage));
+  const lowestMileage = Math.min(...cars.map((car) => car.mileage));
+  const highestMileageCar = cars.find((car) => car.mileage === highestMileage);
+  const lowestMileageCar = cars.find((car) => car.mileage === lowestMileage);
+  const averageMileage = totalMileage / cars.length;
+  return {
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
